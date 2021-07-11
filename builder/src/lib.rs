a7299c4cf06e8a760fdb4cea15a66a89e3381489
Simon A. Berger <simberger@gmail.com>
2021-07-11 11:46:02 +0200

    builder stub

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,12 +1,45 @@
 use proc_macro::TokenStream;
+use quote::{format_ident, quote};
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _input = parse_macro_input!(input as DeriveInput);
-    for attr in _input.attrs {
-        println!("attr: {:?}", attr.path.get_ident());
-    }
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+    let builder_name = format_ident!("{}Builder", name);
+    let fields = match input.data {
+        syn::Data::Struct(ref data) => match data.fields {
+            syn::Fields::Named(ref fields) => &fields.named,
+            _ => unimplemented!(),
+        },
+        // &data.fields,
+        _ => unimplemented!(),
+    };
+    let builder_decl = fields.iter().map(|field| {
+        let name = field.ident.as_ref().unwrap();
+        let ty = &field.ty;
+        quote! {
+            #name: Option<#ty>
+        }
+    });
+    let builder_init = fields.iter().map(|field| {
+        let name = field.ident.as_ref().unwrap();
+        quote! {
+            #name: None
+        }
+    });
 
-    proc_macro::TokenStream::new()
+    let expanded = quote! {
+        impl #name {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    #(#builder_init),*
+                }
+            }
+        }
+        pub struct #builder_name {
+            #(#builder_decl),*
+        }
+    };
+    proc_macro::TokenStream::from(expanded)
 }
